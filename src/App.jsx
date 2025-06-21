3ac8eb4710b3e194cbd20c5d45a90001bceaad8f
Rahul Adari <rahul.adari1@gmail.com>
2025-06-21 13:13:15 +0530

    Enable HashRouter for Render deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 // chat-frontend/src/App.jsx
 
 import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, HashRouter } from "react-router-dom";
 import Login from "./components/Login";
 import ChatUI from "./components/ChatUI";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/chat" element={<ProtectedRoute><ChatUI /></ProtectedRoute>} />
-      <Route path="/" element={<Navigate to="/login" />} />
-      <Route path="*" element={<Navigate to="/login" />} />
-    </Routes>
+    <HashRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/chat" element={
+          <ProtectedRoute>
+            <ChatUI />
+          </ProtectedRoute>
+        } />
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
