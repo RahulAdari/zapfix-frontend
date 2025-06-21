8e5e44e3aea82b51f174fa4bb69f6c0a339ac5bf
Rahul Adari <rahul.adari1@gmail.com>
2025-06-21 13:20:28 +0530

    Fix: removed BrowserRouter to avoid double wrapping

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,14 @@
+// main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./ChatUI.css";
 import "./components/Login.css";
 import "./components/App.css";
-import './index.css'
-import { API_BASE_URL } from "./config"; // adjust path if needed
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
